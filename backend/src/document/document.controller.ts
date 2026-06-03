65f1e999d21196d2f84eb67b547810d303a23421
sarvoday71 <sarvodayjadhav17@gmail.com>
2026-06-03 11:10:40 +0530

    file is now saving in local storage

--- backend/src/document/document.controller.ts
+++ backend/src/document/document.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, HttpCode, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage, memoryStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('document')
 export class DocumentController {
@@ -8,12 +10,14 @@ export class DocumentController {
 
   @Post('upload')
   @HttpCode(200)
-  @UseInterceptors(FileInterceptor('document'))
+  @UseInterceptors(FileInterceptor('document', {
+    storage: memoryStorage(),
+  }))
   async DocumentUpload(
     @UploadedFile() file: Express.Multer.File
   ) {
     console.log(file);
-    return this.documentService.validateTypes(file);
+    return this.documentService.fileProcessing(file);
   }
 
 
--- backend/src/document/document.service.ts
+++ backend/src/document/document.service.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { extname, join } from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class DocumentService {
@@ -14,14 +16,42 @@ export class DocumentService {
         ]
 
         if (!allowedTypes.includes(file.mimetype)) {
-            throw new BadRequestException('Unsupported File Type');
+            return "InvalidType";
         }
 
         const maxSize = 10 * 1024 * 1024;
 
         if (file.size > maxSize) {
-            throw new BadRequestException('File Size Exceeds the maximum limit');
+            return "InvalidSize";
         }
+
+        return "Valid";
+    }
+
+    async fileProcessing(file: Express.Multer.File) {
+        const isValid = this.validateTypes(file);
+
+        if (isValid === "InvalidSize")
+            throw new BadRequestException("File size should be below 10MB");
+
+        if (isValid === "InvalidType")
+            throw new BadRequestException("Please check file type");
+
+        const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+
+        const name = uniqueName + file.originalname;
+
+
+        const filePath = join(
+            process.cwd(),
+            'uploads',
+            name
+        )
+
+        await fs.promises.writeFile(
+            filePath,
+            file.buffer,
+        );
 
         return file;
     }
